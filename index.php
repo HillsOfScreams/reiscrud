424ad59eb3af4e36fbc7e15a4edcf8c0bd7601bb
Necann <1199598@student.roc-nijmegen.nl>
2022-05-24 11:37:47 +0200

    homepagina

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-/* . */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css"/>
+    <title>Reisbureau Travelex</title>
+</head>
+<body>
+    
+    <div id="slider">
+        <figure>
+            <img src="/images/woman_beach.png" alt>
+            <img src="/images/mountains.png" alt>
+            <img src="/images/vacation_beach.png" alt>
+            <img src="/images/egypt.png" alt>
+            <img src="/images/woman_beach.png" alt>
+        </figure>
+    </div>
+
+    <header>
+        <img class="logo1" src="/images/logo_blue.png">
+        <div class="header_row">
+
+        </div>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+
+</body>
+</html>
