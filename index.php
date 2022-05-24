70b0e7e93ee802efdbc8f5e20c4981d9eb62f530
HillsOfScreams <99171750+HillsOfScreams@users.noreply.github.com>
2022-05-24 10:45:01 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-/* . */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>index</title>
+
+<link rel="stylesheet" href="indexMakeUp.css">
+
+
+<div id="slider">
+<figure>
+<img src="https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt>
+<img src="https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt>
+<img src="https://images.pexels.com/photos/2171277/pexels-photo-2171277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt>
+<img src="https://images.pexels.com/photos/853199/pexels-photo-853199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt>
+<img src="https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt>
+</figure>
+</div>
