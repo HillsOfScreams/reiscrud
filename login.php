2512d0b8a93cdb77e2aee0c413950e8ca3e9cab0
Necann <1199598@student.roc-nijmegen.nl>
2022-06-15 09:45:30 +0200

    Front pagina site

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'reiscrud';
+//$DATABASE_NAME = ''; <------------- DB NAAM!!!
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
@@ -34,8 +34,8 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
-            $_SESSION['customer_ID'] = $id;
-            header("Location: booking");
+            $_SESSION['id'] = $id;
+            //header("Location: "); <------------ LOCATIE DUURSTUREN!!!
         } 
         else {
             // Incorrect password
